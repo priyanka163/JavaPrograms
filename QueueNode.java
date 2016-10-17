a31d12134df9e357cfeced665787fd07e052586c
balramcheryala <cheryalabalram123@gmail.com>
2016-10-17 18:22:25 +0530

    Bank program

---
+++ QueueNode.java
@@ -0,0 +1,25 @@
+package com.bridgelabz.programs;
+import com.bridgelabz.util.Utility;
+
+public class QueueNode{
+	int data;
+	QueueNode link;
+
+	public QueueNode(int data, QueueNode node){
+		this.data = data;
+		link = node;
+	}
+	public void setData(int data){
+		this.data = data;
+	}
+	public int getData(){
+		return data;
+	}
+
+	public void setNode(QueueNode node){
+		link = node;
+	}
+	public QueueNode getNode(){
+		return link;
+	}
+}
